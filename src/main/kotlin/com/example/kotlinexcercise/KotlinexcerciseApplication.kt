fa12d79d7898069df4dc4dd0bb4176ebfa1d20fa
David Kovacs <dkovacs@callibrity.com>
2018-11-26 14:52:26 -0500

    First Commit

---
+++ src/main/kotlin/com/example/kotlinexcercise/KotlinexcerciseApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kotlinexcercise
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinexcerciseApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinexcerciseApplication>(*args)
+}
