2687994d7c54d38222bf0bacdad64d13e9912c36
David Kovacs <dkovacs@callibrity.com>
2018-11-26 15:09:23 -0500

    Renamed application file name.

--- src/main/kotlin/com/example/kotlinexcercise/KotlinExerciseApplication.kt
+++ src/main/kotlin/com/example/kotlinexcercise/KotlinExerciseApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class KotlinexcerciseApplication
+class KotlinExerciseApplication
 
 fun main(args: Array<String>) {
-    runApplication<KotlinexcerciseApplication>(*args)
+    runApplication<KotlinExerciseApplication>(*args)
 }
